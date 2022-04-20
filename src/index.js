c2c0fdb40555d2e8024cc57444520647bc308224
Toby Carlo <tobycarlo.c@gmail.com>
2022-04-20 14:48:54 +0100

    setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+
+function createDogListItem(dog) {
+  const li = document.createElement("li");
+  const dogContainer = document.querySelector(".main");
+
+  li.className = "dogs-list__button";
+  li.innerText = dog.name;
+}
